2f66f183cbcc26611db82d811508d7105348a962
Recneps <82240595+0Recneps0@users.noreply.github.com>
2025-06-04 18:42:09 -0700

    Add files via upload
    fixed color variables

--- bashv7.sh
+++ bashv7.sh
@@ -20,6 +20,7 @@ readonly GREEN='\033[0;32m'
 readonly YELLOW='\033[1;33m'
 readonly BLUE='\033[0;34m'
 readonly CYAN='\033[0;36m'
+readonly WHITE='\033[1;37m'
 readonly NC='\033[0m' # No Color
 
 # Color output toggle
